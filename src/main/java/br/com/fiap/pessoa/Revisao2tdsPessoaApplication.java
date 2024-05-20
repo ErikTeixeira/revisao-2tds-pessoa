df6bb56de07f490d8647315f0f7c9204147d11a0
Benefrancis <benefrancis@gmail.com>
2024-05-20 08:09:52 -0300

    inicial

---
+++ src/main/java/br/com/fiap/pessoa/Revisao2tdsPessoaApplication.java
@@ -0,0 +1,13 @@
+package br.com.fiap.pessoa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Revisao2tdsPessoaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Revisao2tdsPessoaApplication.class, args);
+    }
+
+}
